13588c38350c40e16c4455b38bd4f66dcb900b91
aria2 <22f22728@mec.edu.om>
2026-07-01 14:16:36 +0400

    Add medium: task 9

--- Sparktocode C# Fundamentals/Program.cs
+++ Sparktocode C# Fundamentals/Program.cs
@@ -154,6 +154,39 @@ namespace Csharpfundamentals
             Console.WriteLine("The original bill is: " + bill + ", the discount amount is: " + discount + ", and the final amount to pay is: " + final);
             Console.Write("\n");
 
+            // Medium: Task 9
+
+            Console.Write("Enter a number from 1-7: ");
+            int numday = Convert.ToInt32(Console.ReadLine());
+            switch (numday)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+            }
+            Console.Write("\n");
+
 
         }
 }
