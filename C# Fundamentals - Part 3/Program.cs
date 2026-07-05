db97e78bb3436dafca73ccfc451a02bf01d858c9
aria2 <22f22728@mec.edu.om>
2026-07-05 14:36:34 +0400

    Implement solution for Task 1 - Absolute Difference

---
+++ C# Fundamentals - Part 3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace C__Fundamentals___Part_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Easy: Task 1 - Absolute Difference
+
+            Console.WriteLine("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double difference = Math.Abs(num1 - num2);
+            Console.WriteLine("The absolute difference is " + difference);
+
+
+            
+        }
+    }
+}
