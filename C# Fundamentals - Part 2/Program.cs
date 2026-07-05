57fea4568e15a87a48067fe6f753d1b53c786b35
aria2 <22f22728@mec.edu.om>
2026-07-05 13:50:27 +0400

    Add solution for Task 6 - Safe Division Calculator

--- C# Fundamentals - Part 2/Program.cs
+++ C# Fundamentals - Part 2/Program.cs
@@ -94,6 +94,30 @@
             Console.Write("\n");
 
 
+            // Medium: Task 6 - Safe Division Calculator
+
+            try
+            {
+                Console.WriteLine("Enter the first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int result = num1 / num2;
+                Console.WriteLine("Result = " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: Cannot divide by zero");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error: Please enter valid whole numbers");
+            }
+            Console.Write("\n");
+
+
 
         }
     }
