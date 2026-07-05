37b2425f37ed2437d86877b95e9dd5b3c6932dcd
aria2 <22f22728@mec.edu.om>
2026-07-05 13:21:29 +0400

    Add solution for Task 1 - Countdown Timer

---
+++ C# Fundamentals - Part 2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace C__Fundamentals___Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Easy: Task 1 - Countdown Timer
+            Console.WriteLine("Enter a starting number: ");
+            int start = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = start; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("Liftoff!");
+            Console.Write("\n");
+
+
+        }
+    }
+}
