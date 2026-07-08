9fe5d34e9b148d325e94156f48816e40b8ee8cbe
Dalia Al Ghailani <22f22728@mec.edu.om>
2026-07-08 09:08:07 +0400

    Implement solution for Task 1 - Fixed Grades Array

---
+++ C# Fundamentals - Part 5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace C__Fundamentals___Part_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Easy: Task 1 - Fixed Grades Array
+            int[] grades1 = new int[5];
+
+            for (int i = 0; i < grades1.Length; i++)
+            {
+                Console.Write("Enter grade " + (i + 1) + ": ");
+                grades1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nAll grades entered:");
+            foreach (int grade1 in grades1)
+            {
+                Console.WriteLine(grade1);
+            }
+            Console.Write("\n");
+
+
+
+        }
+    }
+}
